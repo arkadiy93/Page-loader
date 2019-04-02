0218833b3447484e4648262f7aaefd4289fdc201
Arkadiy <nvriper@gmail.com>
2019-04-02 21:07:07 +0200

    refactor tests

--- __tests__/initTest.js
+++ __tests__/initTest.js
@@ -1,15 +1,17 @@
 import nock from 'nock';
 import os from 'os';
 import { promises as fs } from 'fs';
+import path from 'path';
 import pageLoader from '../src';
 
 test('first test', async () => {
-  const path = `${os.tmpdir()}`;
+  const dir = `${os.tmpdir()}`;
+  const fileName = 'hexlet-io-courses.html';
   nock('https://hexlet.io')
     .get('/courses')
     .reply(200, 'Hello from Hexlet!');
 
-  await pageLoader(path, 'https://hexlet.io/courses');
-  const data = await fs.readFile(`${path}/lol.html`, 'utf-8');
+  await pageLoader(dir, 'https://hexlet.io/courses');
+  const data = await fs.readFile(path.join(dir, fileName), 'utf-8');
   expect(data).toBe('Hello from Hexlet!');
 });
